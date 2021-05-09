a1533e9e8f2c67a360a22401e0458d694ec8dbfe
beardead1 <66279910+beardead1@users.noreply.github.com>
2021-05-09 16:09:25 +0200

    Add files via upload
    Naprawa przekroczenia wagi i objętosci

--- Problem-plecakowy/Problem-plecakowy/Program.cs
+++ Problem-plecakowy/Problem-plecakowy/Program.cs
@@ -41,32 +41,35 @@ namespace Problem_plecakowy
                 int waga1 = 0;
                 int obj = 0;
 
+
                 //POPRAWIĆ  dać IFa, żeby nie przekraczało wagi i obj.
-                for (int j = 0; waga1 < 1000 && obj < objetosc; j++)
+                for (int j = 0; waga1 <= 1000 && obj < objetosc; j++)
                 {
                     int losowa = rand.Next(10000);
                     paczka_new.Add(paczka[losowa]);
+                    //zabezpieczenie przed dodaniem paczki przekraczającą wagę
+                    if(waga1 + paczka_new[j].Waga > 1000) {break; }
                     waga1 = waga1 + paczka_new[j].Waga;
+                    //zabezpieczenie przed dodaniem paczki przekraczającą objętość
+                    if (obj + (paczka_new[j].Dlugosc * (paczka_new[j].Szerokosc * paczka_new[j].Wysokosc)) > 1614800){ break; }
                     obj = obj + (paczka_new[j].Dlugosc * (paczka_new[j].Szerokosc * paczka_new[j].Wysokosc));
                 }
+                  paczka_new.Remove(paczka_new[paczka_new.Count-1]);
 
 
-            //wyświetlamy loswe paczki do 1000 kg
-                int waga_test = 0;
+            //wyświetlamy losowe paczki do 1000 kg
+            int waga_test = 0;
                 int obj_test = 0;
                 for (int i = 0; i < paczka_new.Count; i++)
                 {
                 Console.WriteLine("ID: " + paczka_new[i].Id + " Waga: " + paczka_new[i].Waga + " Rozmiar: " + paczka_new[i].Wysokosc + "\\" + paczka_new[i].Szerokosc + "\\" + paczka_new[i].Dlugosc);
                 waga_test = waga_test + paczka_new[i].Waga;
                 obj_test = obj_test + paczka_new[i].Dlugosc * (paczka_new[i].Szerokosc * paczka_new[i].Wysokosc);
-
                 }
                 Console.WriteLine("Ilość paczek: ");
                 Console.WriteLine(paczka_new.Count);
                 Console.WriteLine("Waga razem: " + waga_test);
                 Console.WriteLine("Objętość razem: " + obj_test);
-
-
 
         }
     }
