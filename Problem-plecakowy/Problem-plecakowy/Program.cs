24eec8b83476d992ac66f54c3a05db4ffbdd8ecb
beardead1 <66279910+beardead1@users.noreply.github.com>
2021-05-16 15:50:06 +0200

    Pracę z dnia 16.05.2021
    Wykonany plan implementacji 2 do punktu 11 włącznie.

--- Problem-plecakowy/Problem-plecakowy/Program.cs
+++ Problem-plecakowy/Problem-plecakowy/Program.cs
@@ -12,7 +12,7 @@ namespace Problem_plecakowy
             List<Paczka> paczka = new List<Paczka>();
             var lines = System.IO.File.ReadAllLines("baza.csv");
             lines = lines.Skip(1).ToArray();
-
+            int licz_foreach = 0;
             foreach (var item in lines)
             {
                 var values = item.Split(';');
@@ -31,16 +31,90 @@ namespace Problem_plecakowy
             List<Paczka> paczka_new = new List<Paczka>();
 
             int objetosc = 1614800;
-            int[] HM;
+            List<List<Paczka>> HM = new List<List<Paczka>>();
             int HMS = 15;
+            List<int> FP = new List<int>();            //fitness dla każdego rozwiązania
+            List<int> FP_2 = new List<int>();            //fitness dla nowych rozwiązań
+
+            List<Paczka> nowe_rozwiazania = new List<Paczka>();
+
+
+
+            for (int x = 0; x < HMS; x++)
+            {
+                rozwiazania();
+
+                List<Paczka> pomocnicza = new List<Paczka>(paczka_new);
+                //dodaj rozwiązania
+                FP.Add(paczka_new.Count);
+                HM.Add(pomocnicza);
+                paczka_new.Clear();
+            }
+
+
+            foreach (List<Paczka> aPart in HM)
+            {
+                Console.WriteLine("HM next:");
+                foreach (Paczka aPart1 in aPart)
+                {
+                    Console.WriteLine("FP w HM: " + aPart1.Lp);
+                }
+            }
 
 
 
 
-                // dodajemy do listy do wagi 1000 kg
+
+            int HMCR = 70;
+            int iteracje = 100;
+
+
+            List<List<Paczka>> HM_nowe = new List<List<Paczka>>();
+
+            for (int g = 0; g < iteracje; g++)
+            {
+                int r1 = rand.Next(100);
+
+                if (r1 < 100)
+                {
+                    //szukamy nowego rozwiązania z pamięci algorytmu
+                   nowe_rozwiazania.Clear();
+                   rozwiazania_z_naszego();
+                   List<Paczka> pomoc = new List<Paczka>(nowe_rozwiazania);
+                   HM_nowe.Add(pomoc);
+                   FP_2.Add(nowe_rozwiazania.Count); 
+                   
+                } else
+                {
+                    paczka_new.Clear();
+                    rozwiazania();
+                    List<Paczka> pomoc2 = new List<Paczka>(paczka_new);
+                    HM_nowe.Add(pomoc2);
+                    FP_2.Add(paczka_new.Count);
+                }
+            } 
+
+
+            
+
+            foreach (List<Paczka> aPart in HM_nowe)
+            {
+                Console.WriteLine("--------Następna Lista--------:" );
+                Console.WriteLine("** funkcja przystosowania **" + FP_2[licz_foreach]);
+                Console.WriteLine("--------Nowe rozwiązania--------:");
+                foreach (Paczka aPart1 in aPart)
+                {
+                    Console.WriteLine("ID: "+aPart1.Id +" Waga: " +aPart1.Waga + " Dł: " +aPart1.Dlugosc + " Sz.: " +aPart1.Szerokosc + " Wys.: " +aPart1.Wysokosc);
+                }
+                licz_foreach++;
+            }
+
+
+            void rozwiazania() {
+
                 int waga1 = 0;
                 int obj = 0;
-
+                
 
                 //POPRAWIĆ  dać IFa, żeby nie przekraczało wagi i obj.
                 for (int j = 0; waga1 <= 1000 && obj < objetosc; j++)
@@ -48,28 +122,41 @@ namespace Problem_plecakowy
                     int losowa = rand.Next(10000);
                     paczka_new.Add(paczka[losowa]);
                     //zabezpieczenie przed dodaniem paczki przekraczającą wagę
-                    if(waga1 + paczka_new[j].Waga > 1000) {break; }
+                    if (waga1 + paczka_new[j].Waga > 1000) { break; }
                     waga1 = waga1 + paczka_new[j].Waga;
                     //zabezpieczenie przed dodaniem paczki przekraczającą objętość
-                    if (obj + (paczka_new[j].Dlugosc * (paczka_new[j].Szerokosc * paczka_new[j].Wysokosc)) > 1614800){ break; }
+                    if (obj + (paczka_new[j].Dlugosc * (paczka_new[j].Szerokosc * paczka_new[j].Wysokosc)) > 1614800) { break; }
                     obj = obj + (paczka_new[j].Dlugosc * (paczka_new[j].Szerokosc * paczka_new[j].Wysokosc));
                 }
-                  paczka_new.Remove(paczka_new[paczka_new.Count-1]);
+                paczka_new.Remove(paczka_new[paczka_new.Count - 1]);
+            }
 
 
-            //wyświetlamy losowe paczki do 1000 kg
-            int waga_test = 0;
-                int obj_test = 0;
-                for (int i = 0; i < paczka_new.Count; i++)
+            void rozwiazania_z_naszego()
+            {
+
+                int waga1 = 0;
+                int obj = 0;
+
+
+                //POPRAWIĆ  dać IFa, żeby nie przekraczało wagi i obj.
+                for (int j = 0; waga1 <= 1000 && obj < objetosc; j++)
                 {
-                Console.WriteLine("ID: " + paczka_new[i].Id + " Waga: " + paczka_new[i].Waga + " Rozmiar: " + paczka_new[i].Wysokosc + "\\" + paczka_new[i].Szerokosc + "\\" + paczka_new[i].Dlugosc);
-                waga_test = waga_test + paczka_new[i].Waga;
-                obj_test = obj_test + paczka_new[i].Dlugosc * (paczka_new[i].Szerokosc * paczka_new[i].Wysokosc);
+                    int losowa = rand.Next(15);
+                   
+                    nowe_rozwiazania.Add(HM[losowa][rand.Next(HM[losowa].Count)]);
+
+                    //zabezpieczenie przed dodaniem paczki przekraczającą wagę
+                    if (waga1 + nowe_rozwiazania[j].Waga > 1000) { break; }
+                    waga1 = waga1 + nowe_rozwiazania[j].Waga;
+                    //zabezpieczenie przed dodaniem paczki przekraczającą objętość
+                    if (obj + (nowe_rozwiazania[j].Dlugosc * (nowe_rozwiazania[j].Szerokosc * nowe_rozwiazania[j].Wysokosc)) > 1614800) { break; }
+                    obj = obj + (nowe_rozwiazania[j].Dlugosc * (nowe_rozwiazania[j].Szerokosc * nowe_rozwiazania[j].Wysokosc));
                 }
-                Console.WriteLine("Ilość paczek: ");
-                Console.WriteLine(paczka_new.Count);
-                Console.WriteLine("Waga razem: " + waga_test);
-                Console.WriteLine("Objętość razem: " + obj_test);
+                nowe_rozwiazania.Remove(nowe_rozwiazania[nowe_rozwiazania.Count - 1]);
+            }
+
+
 
         }
     }
